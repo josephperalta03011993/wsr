e10427b91979a9d0130320c098c7107bfbe31bc4
Jaryne <man24035@byui.edu>
2025-10-19 19:57:22 +0800

    initial commit

---
+++ resources.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Homepage - find basic information in the WSR Library.">
+  <meta name="author" content="Jaryne Moraleda Manrique, Ryan Manrique"> <!-- add authors -->
+  <title>Homepage | Welfare & Self-Reliance</title>
+
+  <!-- Open Graph tags -->
+  <meta property="og:title" content="WSR Library Resources">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="https://josephperalta03011993.github.io/wsr-website/public/assets/logo.png">
+  <meta property="og:url" content="https://josephperalta03011993.github.io/wsr-website/public/index.html">
+
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="icon" href="src/assets/logo.png" type="image/x-icon">
+
+  <link rel="stylesheet" href="src/css/styles.css">
+
+  <script src="src/js/main.js" defer></script>
+</head>
+
+<body>
+  <header class="site-header">
+    <div class="logo-nav">
+      <section class="logo">
+        <img src="src/assets/logo.png" alt="WSR-LogoTemp">
+      </section>
+      <nav class="main-nav"> <!-- note no hamburger menu yet -->
+        <a href="https://jaryne.github.io/wsr-website-main/public/index.html">Home Page</a>
+        <a href="https://jaryne.github.io/wsr-website-main/public/resources.html">Resources Hub</a>
+        <a href="https://jaryne.github.io/wsr-website-main/public/programs.html" class="dropdown">
+          <span>Programs Menu</span>
+          <span class="material-icons">expand_more</span>
+        </a>
+      </nav>
+    </div>
+    <button class="btn btn-primary">Learn</button> <!-- change function or remove -->
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer class="site-footer">
+    <div class="footer-top">
+      <section class="logo">
+        <img src="src/assets/logo.png" alt="WSR-LogoTemp">
+      </section>
+      <nav class="footer-nav">
+        <a href="#">Contact Us</a>
+        <a href="#">Join Group</a>
+        <a href="#">Latest News</a>
+        <a href="#">Help Center</a>
+        <a href="#">Success Stories</a>
+      </nav>
+      <div class="socials">
+        <a href="#"><span class="material-icons">facebook</span></a>
+        <a href="#"><span class="material-icons">camera_alt</span></a>
+        <a href="#"><span class="material-icons">cloud</span></a>
+        <a href="#"><span class="material-icons">ondemand_video</span></a>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>© 2025 Welfare and Self-Reliance. All rights reserved.</p>
+      <div class="footer-links">
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms of Use</a>
+        <a href="#">Cookie Policy</a>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
