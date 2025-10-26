01b3d6a1870050b50d36b789e1706a899a0a633f
Peralta <joseph.peralta03011993@gmail.com>
2025-10-26 23:05:10 +0800

    update

--- config/database.php
+++ config/database.php
@@ -4,6 +4,11 @@ $username = "root";
 $password = "";
 $dbname = "wsr_website";
 
+// $servername = "localhost";
+// $username = "u414060592_wsr_developer";
+// $password = "?uD2EK7meZZ3!w2";
+// $dbname = "u414060592_wsr_website";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
