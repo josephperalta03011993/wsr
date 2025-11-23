fc00014ac3fcfdbbd6c563d28ee773a3e69209b7
Ryan Manrique <ryan.manrique@gmail.com>
2025-11-23 19:28:26 +0800

    added responsive.css, updated family-services.php

--- views/family-services.php
+++ views/family-services.php
@@ -1,5 +1,44 @@
 <?php include 'header.php'; ?>
   <main>
-    <h1>Family Services</h1>    
+    <h1>Family Services</h1> 
+    <p>Family Services assists leaders in helping members develop emotional self-reliance and overcome obstacles to making and keeping sacred covenants.</p>
+
+<!-- Family Services sections + fade -->
+
+<h2>Responsive Image Gallery</h2>
+<h4>Resize the browser window to see the effect.</h4>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="https://www.w3schools.com/howto/img_5terre.jpg">
+      <img src="https://www.w3schools.com/howto/img_5terre.jpg" alt="Cinque Terre">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="https://www.w3schools.com/howto/img_5terre.jpg">
+      <img src="https://www.w3schools.com/howto/img_forest.jpg" alt="Forest">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="https://www.w3schools.com/howto/img_5terre.jpg">
+      <img src="https://www.w3schools.com/howto/img_lights.jpg" alt="Northern Lights">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="https://www.w3schools.com/howto/img_5terre.jpg">
+      <img src="https://www.w3schools.com/howto/img_mountains.jpg" alt="Mountains">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
   </main>
 <?php include 'footer.php'; ?>
