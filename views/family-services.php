192e6a5b80751622ed47f673433b99014a8ad04e
Jaryne <man24035@byui.edu>
2025-11-25 00:58:14 +0800

    family-services carousel

--- views/family-services.php
+++ views/family-services.php
@@ -3,42 +3,34 @@
     <h1>Family Services</h1> 
     <p>Family Services assists leaders in helping members develop emotional self-reliance and overcome obstacles to making and keeping sacred covenants.</p>
 
-<!-- Family Services sections + fade -->
+<!-- Family Services sections -->
 
 <h2>Responsive Image Gallery</h2>
 <h4>Resize the browser window to see the effect.</h4>
-<div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="https://www.w3schools.com/howto/img_5terre.jpg">
-      <img src="https://www.w3schools.com/howto/img_5terre.jpg" alt="Cinque Terre">
-    </a>
-    <div class="desc">Add a description of the image here</div>
-  </div>
-</div>
-<div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="https://www.w3schools.com/howto/img_5terre.jpg">
-      <img src="https://www.w3schools.com/howto/img_forest.jpg" alt="Forest">
-    </a>
-    <div class="desc">Add a description of the image here</div>
-  </div>
-</div>
-<div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="https://www.w3schools.com/howto/img_5terre.jpg">
-      <img src="https://www.w3schools.com/howto/img_lights.jpg" alt="Northern Lights">
-    </a>
-    <div class="desc">Add a description of the image here</div>
-  </div>
-</div>
-<div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="https://www.w3schools.com/howto/img_5terre.jpg">
-      <img src="https://www.w3schools.com/howto/img_mountains.jpg" alt="Mountains">
-    </a>
-    <div class="desc">Add a description of the image here</div>
-  </div>
-</div>
+    <section class="carousel-wrapper">
+        <div class="carousel" id="carousel">
+            <div class="card">
+                <img src="https://www.w3schools.com/howto/img_5terre.jpg" alt="Product 1">
+                <div class="card-text">
+                    <h3>Wireless Headphones</h3>
+                    <p>Crystal-clear sound, anywhere.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://www.w3schools.com/howto/img_5terre.jpg" alt="Product 2">
+                <div class="card-text">
+                    <h3>Smartwatch X2</h3>
+                    <p>Track your health in style.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://www.w3schools.com/howto/img_5terre.jpg" alt="Product 3">
+                <div class="card-text">
+                    <h3>Eco Water Bottle</h3>
+                    <p>Stay hydrated, stay green.</p>
+                </div>
+            </div>
+    </section>
 
   </main>
 <?php include 'footer.php'; ?>
