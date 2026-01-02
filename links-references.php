d479018c858367a628b9a01e481cd22e6e4eeb3b
Ryan Manrique <ryan.manrique@gmail.com>
2026-01-02 14:29:24 +0800

    Additional update to links-references

--- links-references.php
+++ links-references.php
@@ -4,12 +4,14 @@ NAVIGATION BAR
     - Self-Reliance Courses <a href="sr-couses.php"> (link to sr-courses.php) </a>
         - Courses
         - Download Manuals
-    - Education Support Services
-    - Perpetual Education Fund
-    - Benson Agriculture & Food Scholarships
-    - Employment Services
-    - Humanitarian Services
-    - Family Services
+    - Education Support Services <a href="education-support-services.php"> (link to education-support-services.php) </a>
+        - Education Resources
+        - Scholarships & Grants
+    - Perpetual Education Fund <a href="https://www.churchofjesuschrist.org/life/education/perpetual-education-fund?lang=eng" target="_blank">Perpetual Education Fund</a>
+    - Benson Agriculture & Food Scholarships <a href="https://www.churchofjesuschrist.org/bc/content/ldsorg/welfare/pdf/wsrs-one-pagers-dec-2020/Benson-Scholarship.pdf" target="_blank">Benson Agriculture & Food Scholarships</a>
+    - Employment Services <a href="https://www.churchofjesuschrist.org/life/employment-services?lang=eng" target="_blank">Employment Services</a>
+    - Humanitarian Services <a href="https://philanthropies.churchofjesuschrist.org/humanitarian-services" target="_blank">Humanitarian Services</a>
+    - Family Services <a href="https://www.churchofjesuschrist.org/study/manual/counseling-resources?lang=eng" target="_blank">Family Services</a></h3>
     - My Plan Conference
 
     Success Stories
@@ -20,7 +22,7 @@ NAVIGATION BAR
 
     Slider Image
 
-    Welfare and Self Seliance Services
+    Welfare and Self Reliance Services
     - Learn More
     - Get Involved
 
