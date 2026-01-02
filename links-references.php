04d7870e36b52b2a896b8d754775e1a0c4f92342
Ryan Manrique <ryan.manrique@gmail.com>
2026-01-02 18:30:42 +0800

    Added social media and external links to links-references

--- links-references.php
+++ links-references.php
@@ -11,8 +11,12 @@ NAVIGATION BAR
     - Benson Agriculture & Food Scholarships <a href="https://www.churchofjesuschrist.org/bc/content/ldsorg/welfare/pdf/wsrs-one-pagers-dec-2020/Benson-Scholarship.pdf" target="_blank">Benson Agriculture & Food Scholarships</a>
     - Employment Services <a href="https://www.churchofjesuschrist.org/life/employment-services?lang=eng" target="_blank">Employment Services</a>
     - Humanitarian Services <a href="https://philanthropies.churchofjesuschrist.org/humanitarian-services" target="_blank">Humanitarian Services</a>
+        - Member Focus Initiative 
+        - Child Nutrition Program <a href="https://www.churchofjesuschrist.org/serve/caring/child-nutrition?lang=eng" target="_blank">Child Nutrition Program</a>
+        - Child Nutrition Resources <a href="https://www.churchofjesuschrist.org/life/caring-for-women-and-children-through-improved-nutrition?lang=eng" target="_blank">Child Nutrition Resources</a>
+        - Starting My Business
     - Family Services <a href="https://www.churchofjesuschrist.org/study/manual/counseling-resources?lang=eng" target="_blank">Family Services</a></h3>
-    - My Plan Conference
+    - My Plan Conference <a href="https://www.facebook.com/myplanconference" target="_blank">My Plan Conference</a>
 
     Success Stories
     About Us
@@ -23,28 +27,29 @@ NAVIGATION BAR
     - Government and Community Resources <a href="https://www.churchofjesuschrist.org/study/manual/general-handbook/22-providing-for-temporal-needs?lang=eng&id=title_number99-p288#title_number99" target="_blank">Government and Community Resources</a>
     - Church Resources <a href="https://www.churchofjesuschrist.org/study/manual/general-handbook/22-providing-for-temporal-needs?lang=eng&id=title100-p289#title100" target="_blank">Church Resources</a>
     - Sacred Funds Sacred Responsibility <a href="https://www.churchofjesuschrist.org/tools/help/sacred-funds-sacred-responsibilities?lang=eng" target="_blank">Sacred Funds Sacred Responsibility</a>
+    - Fast Offering Assistance Administration
     - Guide in Administering Medical Assistance <a href="GAMA.pdf" target="_blank">Guide in Administering Medical Assistance</a>
     - Self-Reliance Plan <a href="https://www.churchofjesuschrist.org/bc/content/shared/content/english/pdf/welfare/PD60007387_000_SelfReliancePlan_Member_Web_Interactive.pdf" target="_blank">Self-Reliance Plan</a>
     - Bishops Guide to Self-Reliance Plan <a href="https://www.churchofjesuschrist.org/bc/content/shared/content/english/pdf/welfare/Bishops_Guide_to_the_Self_Reliance_Plan.pdf?download=true" target="_blank">Bishops Guide to Self-Reliance Plan</a>
     - Area Temporal Preparedness Guide <a href="https://ph.churchofjesuschrist.org/acp/bc/Philippines/Area%20Temporal%20Preparedness%20Guidelines/Temporal%20Preparedness%20Guide-Phil.pdf" target="_blank">Area Temporal Preparedness Guide</a>
     - Stake and Ward Emergency Preparedness <a href="https://www.churchofjesuschrist.org/study/manual/emergency-preparedness/06-leader-resources/24-stake-and-ward-emergency-preparedness?lang=eng" target="_blank">Stake and Ward Emergency Preparedness</a>
     - Counselling Resources <a href="https://www.churchofjesuschrist.org/study/manual/counseling-resources?lang=eng" target="_blank">Counselling Resources</a>
-    - QuickReg Registration
+    - QuickReg <a href="https://quickreg.churchofjesuschrist.org/" target="_blank">QuickReg</a> 
 
     Slider Image
 
     Welfare and Self Reliance Services
-    - Learn More <a href="https://www.churchofjesuschrist.org/callings/welfare-self-reliance/resources?lang=eng" target="_blank"> (link to external site) </a>
-    - Get Involved
+    - Learn More <a href="https://www.churchofjesuschrist.org/bc/content/shared/content/english/pdf/welfare/Welfare-Self-Reliance-Products-Services_One_Pagers.pdf?lang=eng" target="_blank"> (link to external site) </a>
+    - Serve as Senior Service Missionary <a href="https://seniormissionary.churchofjesuschrist.org/srsite/?lang=eng" target="_blank"> (link to external site) </a>
 
 
     Building Self-Reliance...
     - WSR Products and Services
-        - Learn More
-    - Self-Reliance Courses
+        - Learn More Learn More <a href="https://www.churchofjesuschrist.org/bc/content/shared/content/english/pdf/welfare/Welfare-Self-Reliance-Products-Services_One_Pagers.pdf?lang=eng" target="_blank"> (link to external site) </a>
+    - Self-Reliance Courses 
         - Sign Up
     - Join a Self-Reliance Group
-        - Contact Us (Find a Group)
+        - Join a Group <<a href="https://quickreg.churchofjesuschrist.org/" target="_blank">Join a Group</a>
 
     Latest Updates and Events
     - Image 1
@@ -53,9 +58,8 @@ NAVIGATION BAR
     - View All
 
     Get Involved with WSR Today
-    - Contact
-    - Join
-
+    - Learn More <a href="https://www.churchofjesuschrist.org/callings/welfare-self-reliance/resources?lang=eng" target="_blank"> (link to external site) </a>
+    - Serve as Senior Service Missionary <a href="https://seniormissionary.churchofjesuschrist.org/srsite/?lang=eng" target="_blank"> (link to external site) </a>
 
     Footer:
     - Contact Us
@@ -65,9 +69,9 @@ NAVIGATION BAR
     - Success Stories
 
     Social Media
-    - Facebook
+    - Facebook <a href="https://www.facebook.com/groups/phwelfareandselfrelianceserves" target="_blank">Facebook</a>
     - Instagram
-    - Cloud
+    - Website <a href="https://ph.churchofjesuschrist.org/" target="_blank">Website</a>
     - Youtube
 
     - Privacy Policy
