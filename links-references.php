a9bd8f74a7a29f012c437d46b2f484a70f776516
Ryan Manrique <ryan.manrique@gmail.com>
2026-01-02 13:57:08 +0800

    Updated link-references

---
+++ links-references.php
@@ -0,0 +1,62 @@
+NAVIGATION BAR
+    Home
+    Programs & Services
+    - Self-Reliance Courses <a href="sr-couses.php"> (link to sr-courses.php) </a>
+        - Courses
+        - Download Manuals
+    - Education Support Services
+    - Perpetual Education Fund
+    - Benson Agriculture & Food Scholarships
+    - Employment Services
+    - Humanitarian Services
+    - Family Services
+    - My Plan Conference
+
+    Success Stories
+    About Us
+    Leader Resources
+    - Specialized Working Groups
+    - QuickReg Registration
+
+    Slider Image
+
+    Welfare and Self Seliance Services
+    - Learn More
+    - Get Involved
+
+
+    Building Self-Reliance...
+    - WSR Products and Services
+        - Learn More
+    - Self-Reliance Courses
+        - Sign Up
+    - Join a Self-Reliance Group
+        - Contact Us (Find a Group)
+
+    Latest Updates and Events
+    - Image 1
+    - Image 2
+    - Image 3
+    - View All
+
+    Get Involved with WSR Today
+    - Contact
+    - Join
+
+
+    Footer:
+    - Contact Us
+    - Join Group
+    - Latest News
+    - Help Center
+    - Success Stories
+
+    Social Media
+    - Facebook
+    - Instagram
+    - Cloud
+    - Youtube
+
+    - Privacy Policy
+    - Terms of Use 
+    - Cookie Policy
