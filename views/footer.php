48eee27ee21e4c3d89046861b91a06e8863ce886
Ryan Manrique <ryan.manrique@gmail.com>
2026-02-18 23:12:33 +0800

    Updated footer

--- views/footer.php
+++ views/footer.php
@@ -6,19 +6,19 @@
       <nav class="footer-nav">
         <a href="#">Contact Us</a>
         <a href="https://quickreg.churchofjesuschrist.org/" target="_blank">Join Group</a>
-        <a href="https://newsroom.churchofjesuschrist.org/" target="_blank">Latest News</a>
+        <a href="https://news-ph.churchofjesuschrist.org/" target="_blank">Latest News</a>
         <a href="#">Help Center</a>
         <a href="#">Success Stories</a>
       </nav>
       <div class="socials">
         <a href="https://www.facebook.com/groups/549825222915998" target="_blank"><span class="material-icons">facebook</span></a>
-        <a href="#"><span class="material-icons">camera_alt</span></a>
-        <a href="#"><span class="material-icons">cloud</span></a>
-        <a href="#"><span class="material-icons">ondemand_video</span></a>
+        <a href="https://www.youtube.com/@LDSPhilippines/featured"><span class="material-icons">camera_alt</span></a>
+        <a href="https://ph.churchofjesuschrist.org/"><span class="material-icons">cloud</span></a>
+        <a href="https://www.youtube.com/watch?v=ccVIxq9bfHw"><span class="material-icons">ondemand_video</span></a>
       </div>
     </div>
     <div class="footer-bottom">
-      <p>© 2025 Welfare and Self-Reliance. All rights reserved.</p>
+      <p>© 2026 Welfare and Self-Reliance Services Philippines. All rights reserved.</p>
       <div class="footer-links">
         <a href="#">Privacy Policy</a>
         <a href="#">Terms of Use</a>
